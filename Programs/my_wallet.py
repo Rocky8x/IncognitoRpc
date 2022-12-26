e6646f0211b4085b28e6b0bcd0a5d8f9b5890f2d
Rocky <madfatcat88@gmail.com>
2022-12-26 09:25:14 +0700

    remove breakpoint and unuse code in wallet

--- Programs/my_wallet.py
+++ Programs/my_wallet.py
@@ -7,16 +7,3 @@ from Objects.NodeObject import Node
 mfc = Node(url="https://mfc88.ddns.net/fn")
 accounts = AccountGroup().load_from_file('./Programs/keys').attach_to_node(mfc)
 acc0 = accounts[0]
-
-def find_unstaked():
-    bbsd = mfc.get_beacon_best_state_detail_info()
-    unstaked = []
-    for acc in accounts:
-        if bbsd.get_auto_staking_committees(acc) is None:
-            unstaked.append(acc)
-    return unstaked
-
-def staked_unstaked():
-    for acc in find_unstaked():
-        tx = acc0.stake_someone_reward_me(acc,auto_re_stake=True)
-        td = mfc.get_tx_by_hash(tx.get_tx_id())
