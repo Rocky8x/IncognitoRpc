b98d66137a8bb116c20b4c38a59c21f12b2a28fb
Rocky <madfatcat88@gmail.com>
2023-02-20 07:58:35 +0700

    cleanup

--- ApiTesting/__init__.py
+++
@@ -1,4 +0,0 @@
-END_POINT = 'http://51.161.119.66:9009'
-END_POINT_MAINNET = 'https://api-coinservice.incognito.org/'
-# END_POINT_MAINNET_V2 = 'https://api-coinservice-beta.incognito.org'
-END_POINT_MAINNET_V2 = 'http://51.161.119.66:8092'
