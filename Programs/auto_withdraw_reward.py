fda3c23fc3b951ee957246f685a96f4fe45ef96a
Rocky <madfatcat88@gmail.com>
2023-04-21 10:55:59 +0700

    update

--- Programs/auto_withdraw_reward.py
+++ Programs/auto_withdraw_reward.py
@@ -3,7 +3,7 @@ import os
 from Objects.AccountObject import AccountGroup
 from Objects.NodeObject import Node
 
-full_node = Node(url=os.getenv("FULL_NODE", "https://mfc88.ddns.net/fn"))
+full_node = Node(url=os.getenv("FULL_NODE", "https://rocky8x.sytes.net/fn"))
 accounts = AccountGroup().load_from_file('./Programs/keys').attach_to_node(full_node)
 acc0 = accounts[0]
 retry = 3
@@ -13,7 +13,7 @@ amount=int(400e9)
 while retry:
     try:
         if acc0.stk_get_reward_amount() > amount:
-            tx = acc0.stk_withdraw_reward_to_me(tx_fee=tx_fee).req_to(full_node).get_transaction_by_hash()
+            tx = acc0.stk_withdraw_reward_to_me(tx_fee=tx_fee).get_transaction_by_hash()
             if tx.is_confirmed():
                 break
             else:
